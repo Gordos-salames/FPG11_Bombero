91553105c2c61fd41f9e2761ce898f08863ddba1
GuiltySpark05 <rottafalejandro@gmail.com>
2023-10-28 16:22:33 -0300

    Add SQL

---
+++ pfg11_bomberos.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-10-2023 a las 21:21:57
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pfg11_bomberos`
+--
+CREATE DATABASE IF NOT EXISTS `pfg11_bomberos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `pfg11_bomberos`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bombero`
+--
+
+CREATE TABLE `bombero` (
+  `idBombero` int(11) NOT NULL,
+  `dni` varchar(8) NOT NULL,
+  `nombre_apellido` varchar(50) NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `celular` varchar(15) NOT NULL,
+  `g_Sanguineo` varchar(3) NOT NULL,
+  `codBrigada` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `brigada`
+--
+
+CREATE TABLE `brigada` (
+  `codBrigada` int(11) NOT NULL,
+  `brigadaName` varchar(20) NOT NULL,
+  `especialidad` varchar(30) NOT NULL,
+  `libre` tinyint(1) NOT NULL,
+  `nro_cuartel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuartel`
+--
+
+CREATE TABLE `cuartel` (
+  `codCuartel` int(11) NOT NULL,
+  `nomCuartel` varchar(20) NOT NULL,
+  `direccion` varchar(30) NOT NULL,
+  `coord_X` int(11) NOT NULL,
+  `coord_Y` int(11) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `correo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `siniestro`
+--
+
+CREATE TABLE `siniestro` (
+  `código` int(11) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `fecha_siniestro` date NOT NULL,
+  `coord_X` int(11) NOT NULL,
+  `coord_Y` int(11) NOT NULL,
+  `detalles` text NOT NULL,
+  `fecha_res` date NOT NULL,
+  `puntuacion` int(11) NOT NULL,
+  `codBrigada` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bombero`
+--
+ALTER TABLE `bombero`
+  ADD PRIMARY KEY (`idBombero`),
+  ADD KEY `codBrigada` (`codBrigada`);
+
+--
+-- Indices de la tabla `brigada`
+--
+ALTER TABLE `brigada`
+  ADD PRIMARY KEY (`codBrigada`),
+  ADD KEY `nro_cuartel` (`nro_cuartel`);
+
+--
+-- Indices de la tabla `cuartel`
+--
+ALTER TABLE `cuartel`
+  ADD PRIMARY KEY (`codCuartel`);
+
+--
+-- Indices de la tabla `siniestro`
+--
+ALTER TABLE `siniestro`
+  ADD PRIMARY KEY (`código`),
+  ADD KEY `codBrigada` (`codBrigada`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `bombero`
+--
+ALTER TABLE `bombero`
+  MODIFY `idBombero` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `brigada`
+--
+ALTER TABLE `brigada`
+  MODIFY `codBrigada` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cuartel`
+--
+ALTER TABLE `cuartel`
+  MODIFY `codCuartel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `siniestro`
+--
+ALTER TABLE `siniestro`
+  MODIFY `código` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `bombero`
+--
+ALTER TABLE `bombero`
+  ADD CONSTRAINT `bombero_ibfk_1` FOREIGN KEY (`codBrigada`) REFERENCES `brigada` (`codBrigada`);
+
+--
+-- Filtros para la tabla `brigada`
+--
+ALTER TABLE `brigada`
+  ADD CONSTRAINT `brigada_ibfk_1` FOREIGN KEY (`nro_cuartel`) REFERENCES `cuartel` (`codCuartel`);
+
+--
+-- Filtros para la tabla `siniestro`
+--
+ALTER TABLE `siniestro`
+  ADD CONSTRAINT `siniestro_ibfk_1` FOREIGN KEY (`codBrigada`) REFERENCES `brigada` (`codBrigada`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
